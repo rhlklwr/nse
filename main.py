f92fd9c15a0b7079e94a736c7d902dcf61da6aae
rhlklwr <kalwar.rahul@gmail.com>
2020-03-14 19:14:11 +0530

    update on firestore

---
+++ main.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from nse import *
+
+cred = credentials.Certificate("./ServiceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+batch = db.batch()
+
+# Set the data
+doc_ref_top_gainers = db.collection(u'nse').document(u'top_gainers')
+doc_ref_top_losers = db.collection(u'nse').document(u'top_losers')
+doc_ref_advances_declines = db.collection(u'nse').document(u'advances_declines')
+
+# write to database
+batch.update(doc_ref_top_gainers, {u'top_gainers': top_gainers()})
+batch.update(doc_ref_top_losers, {u'top_losers': top_losers()})
+batch.update(doc_ref_advances_declines, {u'advances_declines': adv_dec()})
+
+# commit batch
+batch.commit()
---
+++ nse.py
@@ -0,0 +1,26 @@
+from nsetools import Nse
+from pprint import pprint
+import time
+
+nse = Nse()
+print(nse)
+
+
+def top_gainers():
+    return nse.get_top_gainers()
+
+
+def top_losers():
+    return nse.get_top_losers()
+
+
+def adv_dec():
+    return nse.get_advances_declines()
+
+
+if __name__ == "__main__":
+    print(
+        top_gainers(),
+        top_losers(),
+        adv_dec()
+    )
