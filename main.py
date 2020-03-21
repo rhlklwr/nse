9422125901400937d6139358fbafe3cc99efb4cd
rhlklwr <kalwar.rahul@gmail.com>
2020-03-21 17:13:25 +0530

    Final code with comment

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+# This is an production code !!!
+
+# Importing relevant package for Firebase, NSE and Time module
 import firebase_admin
 from firebase_admin import credentials, firestore
-from nse import *
-import time
+from nsetools import Nse
+from time import ctime, time, sleep
 
-current_time = time.ctime(time.time()).split()
+# Instantiate NSE class
+nse = Nse()
+
+# Calculation of current time and split it to get the list
+current_time = ctime(time()).split()
+
+# Authentication with Firebase
 cred = credentials.Certificate("./ServiceAccountKey.json")
 app = firebase_admin.initialize_app(cred)
 
+# Instantiate Firestore class
 db = firestore.client()
 
+# Instantiate batch class to update data in single batch
 batch = db.batch()
 
-# Set the data
+# Set the data structure
 doc_ref_top_gainers = db.collection(u'nse').document(u'top_gainers')
 doc_ref_top_losers = db.collection(u'nse').document(u'top_losers')
 doc_ref_advances_declines = db.collection(u'nse').document(u'advances_declines')
@@ -21,11 +32,12 @@ while True:
     if (current_time[0][:3].lower() in ['mon', 'tue', 'wed', 'thu', 'fri']) \
             and \
             (9 < int(current_time[0][:2]) < 17):
-        batch.update(doc_ref_top_gainers, {u'top_gainers': top_gainers()})
-        batch.update(doc_ref_top_losers, {u'top_losers': top_losers()})
-        batch.update(doc_ref_advances_declines, {u'advances_declines': adv_dec()})
-
+        batch.update(doc_ref_top_gainers, {u'top_gainers': nse.get_top_gainers()})
+        batch.update(doc_ref_top_losers, {u'top_losers': nse.get_top_losers()})
+        batch.update(doc_ref_advances_declines, {u'advances_declines': nse.get_advances_declines()})
         # commit batch
         batch.commit()
-        time.sleep(600)
-        current_time = time.ctime(time.time()).split()
+        print(nse)
+        sleep(600)
+        current_time = ctime(time()).split()
+
--- nse.py
+++ nse.py
@@ -1,7 +1,13 @@
+# This is an test code and will not be deploy in production.
+
+# import relevant package for National stock exchange
 from nsetools import Nse
 
+# instantiate NSE class
 nse = Nse()
 print(nse)
+
+# function to get the data
 
 
 def top_gainers():
